d6892f1805cdaf3886f95f534a398bd66970d427
Sadik Hasin Baidya <sadik22319@gmail.com>
2026-09-12 01:29:40 +0530

    Living india

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
         login: resolve(process.cwd(), 'login.html'),
         profile: resolve(process.cwd(), 'profile.html'),
         auth2: resolve(process.cwd(), 'auth2.html'),
-        auth3: resolve(process.cwd(), 'auth3.html')
+        auth3: resolve(process.cwd(), 'auth3.html'),
+        admin: resolve(process.cwd(), 'admin.html')
       }
     }
   }
